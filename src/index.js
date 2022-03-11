7d6763ed5233a99c54f98014fd7460f323c5a920
Dmytro Rudnytskykh <rudnitskih@gmail.com>
2022-03-11 18:36:37 +0200

    Don't send errors on localhost

--- src/index.js
+++ src/index.js
@@ -6,15 +6,18 @@ import reportWebVitals from './reportWebVitals';
 import * as Sentry from "@sentry/react";
 import { BrowserTracing } from "@sentry/tracing";
 
-Sentry.init({
-  dsn: "https://c3f52d7fcaaf44a1bb02480fcbec1d12@o1164917.ingest.sentry.io/6254316",
-  integrations: [new BrowserTracing()],
 
-  // Set tracesSampleRate to 1.0 to capture 100%
-  // of transactions for performance monitoring.
-  // We recommend adjusting this value in production
-  tracesSampleRate: 1.0,
-});
+if (process.env.NODE_ENV === 'production') {
+  Sentry.init({
+    dsn: "https://c3f52d7fcaaf44a1bb02480fcbec1d12@o1164917.ingest.sentry.io/6254316",
+    integrations: [new BrowserTracing()],
+
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    tracesSampleRate: 1.0,
+  });
+}
 
 ReactDOM.render(
   <Sentry.ErrorBoundary fallback={<p>An error has occurred 😢</p>}>
