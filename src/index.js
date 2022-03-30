303c0f5bb3627fa014ab9b1fd147442c86ecd8cf
Dmytro Rudnytskykh <rudnitskih@gmail.com>
2022-03-30 14:14:36 +0300

    Optimize dependencies

--- src/index.js
+++ src/index.js
@@ -2,11 +2,8 @@ import React from 'react';
 import ReactDOM from "react-dom";
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import * as Sentry from "@sentry/react";
 import { BrowserTracing } from "@sentry/tracing";
-
-console.log('---render.com---');
 
 if (process.env.NODE_ENV === 'production') {
   Sentry.init({
@@ -26,8 +23,3 @@ ReactDOM.render(
   </Sentry.ErrorBoundary>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
