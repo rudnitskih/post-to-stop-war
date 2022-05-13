3684695ddf9193dfc385122f8437d06c347730de
Dmytro Rudnytskykh <rudnitskih@gmail.com>
2022-05-13 19:37:05 +0300

    Make fallback to ukraninan messages

--- src/utils/urlUtils.js
+++ src/utils/urlUtils.js
@@ -2,35 +2,6 @@ export const getQueryParam = (param) => {
   return (new URLSearchParams(window.location.search)).get(param);
 }
 
-export const setSiteLang = (siteLang) => {
-  let pathname = `/${siteLang}`;
-
-  if (getSelectedCountry()) {
-    pathname += `/${getSelectedCountry()}`;
-  }
-
-  setPathname(pathname);
-}
-
-export const setSelectedCountry = (selectedCountry) => {
-  setPathname(`/${getSiteLang()}/${selectedCountry}`);
-}
-
 export const getSiteLang = () => {
   return (window.location.pathname.split('/')[1] || 'en').toLowerCase();
-}
-
-export const getSelectedCountry = () => {
-  return window.location.pathname.split('/')[2]?.toUpperCase();
-}
-
-const setPathname = (pathname) => {
-  const {protocol, host, search} = window.location;
-  let newUrl = `${protocol}//${host}${pathname.toLowerCase()}`;
-
-  if (search) {
-    newUrl += `${search}`;
-  }
-
-  window.history.pushState({path: newUrl}, '', newUrl);
 }
