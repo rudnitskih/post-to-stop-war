7ecd3babaa466ec5cffc343ba059762a30cbff10
Dmytro Rudnytskykh <rudnitskih@gmail.com>
2022-05-26 13:26:26 +0300

    Log click change

--- src/Tags/Tags.js
+++ src/Tags/Tags.js
@@ -2,10 +2,20 @@ import React, {Component} from 'react';
 import s from './Tags.module.scss';
 import classNames from "classnames";
 import {t} from "../utils/translate";
+import {logEvent} from "../utils/anayliticsUtils";
 
 export class Tags extends Component {
   render() {
     const {onChange, tags, selectedTag} = this.props;
+
+    const onClick = (tag) => {
+      const newTag = selectedTag === tag ? null : tag;
+
+      onChange(newTag);
+      logEvent('CLICK_TAG', {
+        value: newTag || 'UNSELECT',
+      })
+    }
 
     return tags.length > 1 ? (
       <ul className={s.root}>
@@ -15,7 +25,7 @@ export class Tags extends Component {
               <li key={tag}>
                 <button
                   className={classNames(s.tag, {[s.active]: selectedTag === tag})}
-                  onClick={() => selectedTag === tag ? onChange(null) : onChange(tag)}
+                  onClick={onClick}
                 >
                   {t(tag)}
                 </button>
